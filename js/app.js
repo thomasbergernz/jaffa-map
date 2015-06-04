182a4c25eb9667dea54eb7daa5b405424d26ae81
Thomas Berger <thomasbe@gmail.com>
2015-06-05 11:44:19 +1200

    refactoring using class instead of id in css context

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ var ViewModel = function() {
 	var self = this;
 	var map, bounds, infowindow; // initialize the required variables for Google map
 
-    self.apiUnavailable = ko.observable(false);
+  self.apiUnavailable = ko.observable(false);
 
 	/** Initialize map and create map markers and push them to the Model.
 	  */
@@ -33,27 +33,28 @@ var ViewModel = function() {
           disableDefaultUI: true,
           mapTypeId: google.maps.MapTypeId.TERRAIN
         };
-        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-        bounds = new google.maps.LatLngBounds();
-        infowindow = new google.maps.InfoWindow({
-          content: null
-        });
-
-	    if(typeof window.google === 'object' && typeof window.google.maps === 'object') { // verify that we have a map object to work with
+      // map = new google.maps.Map(document.getElementsByClassName('map-canvas'), mapOptions);
+      map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+      bounds = new google.maps.LatLngBounds();
+      infowindow = new google.maps.InfoWindow({
+        content: null
+      });
+      // verify that we have a map object to work with
+	    if(typeof window.google === 'object' && typeof window.google.maps === 'object') {
 		    // ajax call
-			// get the data from the 3rd party API
-	        $.getJSON(apiURL, function(data) {
-		        var items = data.response.groups[0].items;
-		        //console.log(items);
-		        items.sort(function(a,b){ // sort by name ascending http://www.javascriptkit.com/javatutors/arraysort2.shtml
-			        var nameA=a.venue.name.toLowerCase(), nameB=b.venue.name.toLowerCase();
-					if(nameA < nameB) return -1;
-					if(nameA > nameB) return 1;
-					return 0;
-				});
-				// create marker objects populated with 3rd party api data
-				var itemsLength = items.length;
-		        for (var i = 0; i < itemsLength; i++) {
+        // get the data from the 3rd party API
+        $.getJSON(apiURL, function(data) {
+	        var items = data.response.groups[0].items;
+	        //console.log(items);
+	        items.sort(function(a,b){ // sort by name ascending http://www.javascriptkit.com/javatutors/arraysort2.shtml
+		        var nameA=a.venue.name.toLowerCase(), nameB=b.venue.name.toLowerCase();
+  					if(nameA < nameB) return -1;
+  					if(nameA > nameB) return 1;
+  					return 0;
+			    });
+  				// create marker objects populated with 3rd party api data
+  				var itemsLength = items.length;
+  		        for (var i = 0; i < itemsLength; i++) {
 		            var coordinates = new google.maps.LatLng(items[i].venue.location.lat, items[i].venue.location.lng);
 		            // construct a string containing the title, category and address of the location
 		            // user input 'restaurant' will produce results even if not in the title of the location
@@ -73,100 +74,100 @@ var ViewModel = function() {
 		              rating: myUtils.toFriendlyString(items[i].venue.rating),
 		              address: myUtils.toFriendlyString(items[i].venue.location.address),
 		              category: myUtils.toFriendlyString(items[i].venue.categories[0].name)
-		        	});
+		        	   });
 
-					/** add event listener for each marker
-					  * compile content for infoWindow
-					  */
-			        google.maps.event.addListener(marker, 'click', function() {
-						var self = this;
-						self.setAnimation(google.maps.Animation.DROP);
-						infowindow.setContent("<h5><span class=\"label label-primary\">" + self.category +"</span>" + "&nbsp;"+
-							"<span class=\"label label-default\">" + self.rating +"</span></h5><h4>" + self.title + "</h4>" +
-							"<p>" + self.address + "</p>" + "<p><a href=tel:" + self.phone + ">" + self.phone_formatted + "</a></p>" +
-							"<p><a href=" + self.url + ">" + self.url + "</a></p>" +
-							"<p><img src='"+foursquarelogo+"'alt='foursquare' width='200'></p>" +
-							"<p><img src='"+self.streetview+"'alt='foursquare'></p>");
+                /** add event listener for each marker
+      					  * compile content for infoWindow
+      					  */
+                google.maps.event.addListener(marker, 'click', function() {
+      						var self = this;
+      						self.setAnimation(google.maps.Animation.DROP);
+      						infowindow.setContent("<h5><span class=\"label label-primary\">" + self.category +"</span>" + "&nbsp;"+
+      							"<span class=\"label label-default\">" + self.rating +"</span></h5><h4>" + self.title + "</h4>" +
+      							"<p>" + self.address + "</p>" + "<p><a href=tel:" + self.phone + ">" + self.phone_formatted + "</a></p>" +
+      							"<p><a href=" + self.url + ">" + self.url + "</a></p>" +
+      							"<p><img src='"+foursquarelogo+"'alt='foursquare' width='200'></p>" +
+      							"<p><img src='"+self.streetview+"'alt='foursquare'></p>");
+      
+      						// open infoWindow and clear markers
+      						infowindow.open(map, self);
+      						clearHighlight();
+      
+      						// Modify marker (and list) to show selected status.
+      						self.highlight(true);
+      
+      						// Move map viewport to center selected item.
+      						map.panTo(self.position);
+    			      }); // END google.maps.event.addListener(marker, 'click', function ()
+    
+    			        /** click event to close infowindow
+    			          * This function will clear the selected location
+    			          */
+    			        google.maps.event.addListener(infowindow, 'closeclick', function() {
+    			          clearHighlight();
+    			        });
+    
+    			        // Modify map to show all markers
+    			        bounds.extend(coordinates);
+    
+    			        //Add marker to array
+    			        Model.locations.push(marker);
 
-						// open infoWindow and clear markers
-						infowindow.open(map, self);
-						clearHighlight();
+		            } // END for loop
 
-						// Modify marker (and list) to show selected status.
-						self.highlight(true);
-
-						// Move map viewport to center selected item.
-						map.panTo(self.position);
-			        }); // END google.maps.event.addListener(marker, 'click', function ()
-
-			        /** click event to close infowindow
-			          * This function will clear the selected location
-			          */
-			        google.maps.event.addListener(infowindow, 'closeclick', function() {
-			          clearHighlight();
-			        });
-
-			        // Modify map to show all markers
-			        bounds.extend(coordinates);
-
-			        //Add marker to array
-			        Model.locations.push(marker);
-
-			    } // END for loop
-
-			map.fitBounds(bounds);
-			map.setCenter(bounds.getCenter());
-
-		    }).fail(function(e) { // handle error in case we can't get the data from the API
-			    console.log('Could not get data from API!');
-			    self.apiUnavailable(true);
-			    $("#map-canvas").css({"display":"none"}); // clean UI
-		    });
-		    // END ajax call
-		    // else no google maps object found, display error message
-		} else {
-			self.apiUnavailable(true);
-			$("#map-canvas").css({"display":"none"});
-  		}
-	}(); // END initialize function
-
-	/** FILTER and return items that match query
-	 *  http://www.strathweb.com/2012/07/knockout-js-pro-tips-working-with-observable-arrays/
-	 */
-	self.filterText = ko.observable(''); // reset locations filter
-	function combiFilter(marker) {
-			return marker.searchblob.toLowerCase().indexOf(self.filterText().toLowerCase()) !== -1;
-    }
-    self.myFilteredResults = ko.computed(function() {
-	    return ko.utils.arrayFilter(Model.locations(), combiFilter);
-    }).extend({ throttle: 500 });
-
-	/** Subscribing to the myFilteredResults changes will allow for showing or hiding
-	 * the associated markers on the map itself using Google Maps API
-	 */
-    self.myFilteredResults.subscribe(function() {
-      var difference = ko.utils.compareArrays(Model.locations(), self.myFilteredResults());
-      ko.utils.arrayForEach(difference, function(marker) {
-        if (marker.status === 'deleted') {
-          marker.value.setMap(null);
-        } else {
-          marker.value.setMap(map);
+        			map.fitBounds(bounds);
+        			map.setCenter(bounds.getCenter());
+        
+        }).fail(function(e) { // handle error in case we can't get the data from the API
+    			    console.log('Could not get data from API!');
+    			    self.apiUnavailable(true);
+    			    $(".map-canvas").css({"display":"none"}); // clean UI
+  		  });
+  		    // END ajax call
+  		    // else no google maps object found, display error message
+      } else {
+        self.apiUnavailable(true);
+        $(".map-canvas").css({"display":"none"});
         }
+    }(); // END initialize function
+
+  	/** FILTER and return items that match query
+  	 *  http://www.strathweb.com/2012/07/knockout-js-pro-tips-working-with-observable-arrays/
+  	 */
+  	self.filterText = ko.observable(''); // reset locations filter
+  	function combiFilter(marker) {
+  			return marker.searchblob.toLowerCase().indexOf(self.filterText().toLowerCase()) !== -1;
+      }
+      self.myFilteredResults = ko.computed(function() {
+  	    return ko.utils.arrayFilter(Model.locations(), combiFilter);
+      }).extend({ throttle: 500 });
+  
+  	/** Subscribing to the myFilteredResults changes will allow for showing or hiding
+  	 * the associated markers on the map itself using Google Maps API
+  	 */
+      self.myFilteredResults.subscribe(function() {
+        var difference = ko.utils.compareArrays(Model.locations(), self.myFilteredResults());
+        ko.utils.arrayForEach(difference, function(marker) {
+          if (marker.status === 'deleted') {
+            marker.value.setMap(null);
+          } else {
+            marker.value.setMap(map);
+          }
+        });
       });
-    });
-
-	// Highlight map marker if list item is clicked.
-	self.selectItem = function(listItem) {
-		google.maps.event.trigger(listItem, 'click');
-	};
-
-	/** reset all markers, clear highlight in list
-	 */
-	function clearHighlight() {
-		for(var i = 0; i < Model.locations().length; i++) {
-			Model.locations()[i].highlight(false);
-		}
-	}
+  
+  	// Highlight map marker if list item is clicked.
+  	self.selectItem = function(listItem) {
+  		google.maps.event.trigger(listItem, 'click');
+  	};
+  
+  	/** reset all markers, clear highlight in list
+  	 */
+  	function clearHighlight() {
+  		for(var i = 0; i < Model.locations().length; i++) {
+  			Model.locations()[i].highlight(false);
+  		}
+  	}
 }; // END ViewModel
 
 ko.applyBindings(new ViewModel());
